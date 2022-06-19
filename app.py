148f714f04085dc426625afb3d239dd5db55554e
Porzeraklon <90037576+Porzeraklon@users.noreply.github.com>
2022-06-19 19:11:58 +0200

    sort fix

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ def home():
         for data in response_stops[today]['stops']:
             if data['zoneName'] != None:
                 zone_list.append(data['zoneName'])
+        zone_list.sort()
         zone_list_fix = set(zone_list)
         zone_list_fix.sort()
         return render_template("zone.html", zone_list=zone_list_fix)
